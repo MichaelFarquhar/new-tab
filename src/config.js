cad5280c22015b96e43825091b9140aa56052222
Michael <michaelfarquhar4@gmail.com>
2023-05-10 21:21:38 -0400

    Add Time component and config file

---
+++ src/config.js
@@ -0,0 +1,14 @@
+// Change your global settings here
+// --------------------------------
+
+// Change title of tab
+export const SITE_TITLE = 'New Tab';
+
+// Switch between 'light' or 'dark' theme
+export const SITE_THEME = 'dark';
+
+// Show/hide the time component
+export const SITE_SHOW_TIME = true;
+
+// Choose if the time is set to 12hr or 24hr format
+export const SITE_TIME_FORMAT_12HR = true;
