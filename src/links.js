0cd4cf02dfaefd1a08a3235504e4c540afe01cd0
Michael <michaelfarquhar4@gmail.com>
2023-05-10 22:31:47 -0400

    Add Featured Links section

---
+++ src/links.js
@@ -0,0 +1,69 @@
+export default {
+    featured: [
+        'https://www.google.com',
+        'https://www.youtube.com',
+        'https://www.facebook.com',
+        'https://www.baidu.com',
+        'https://www.wikipedia.org',
+        'https://www.qq.com',
+        'https://www.amazon.com',
+        'https://www.twitter.com',
+        'https://www.instagram.com',
+        'https://www.sohu.com',
+        'https://www.sina.com.cn',
+        'https://www.reddit.com',
+        'https://www.netflix.com',
+        'https://www.microsoft.com',
+        'https://www.office.com',
+    ],
+    categories: [
+        {
+            title: 'News',
+            links: [
+                'https://www.cnn.com',
+                'https://www.bbc.com',
+                'https://www.nytimes.com',
+            ],
+        },
+        {
+            title: 'Social Media',
+            links: [
+                'https://www.instagram.com',
+                'https://www.facebook.com',
+                'https://www.twitter.com',
+            ],
+        },
+        {
+            title: 'Shopping',
+            links: [
+                'https://www.amazon.com',
+                'https://www.ebay.com',
+                'https://www.walmart.com',
+            ],
+        },
+        {
+            title: 'Search Engines',
+            links: [
+                'https://www.google.com',
+                'https://www.bing.com',
+                'https://www.yahoo.com',
+            ],
+        },
+        {
+            title: 'Video Sharing',
+            links: [
+                'https://www.youtube.com',
+                'https://www.vimeo.com',
+                'https://www.tiktok.com',
+            ],
+        },
+        {
+            title: 'Education',
+            links: [
+                'https://www.wikipedia.org',
+                'https://www.khanacademy.org',
+                'https://www.coursera.org',
+            ],
+        },
+    ],
+};
